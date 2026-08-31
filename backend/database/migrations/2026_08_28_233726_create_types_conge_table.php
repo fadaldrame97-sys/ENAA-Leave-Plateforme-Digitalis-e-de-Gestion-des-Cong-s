3d5623a7e0b9e7a16516583e4b4f6cb1bb7c6c34
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-08-31 10:50:07 +0100

    Remplire les clonnes de table types_conge

--- backend/database/migrations/2026_08_28_233726_create_types_conge_table.php
+++ backend/database/migrations/2026_08_28_233726_create_types_conge_table.php
@@ -6,20 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('types_conge', function (Blueprint $table) {
             $table->id();
+            $table->string('nom');
+            $table->float('jour_reste_par_mois')->default(0);
+            $table->boolean('necessite_justificatif')->default(false);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('types_conge');
