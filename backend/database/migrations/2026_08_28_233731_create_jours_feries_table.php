7a7f62d20dda5352c30f31338395b6c11c520d10
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-08-31 11:28:53 +0100

    Remplire les clonnes de table jours_feries

--- backend/database/migrations/2026_08_28_233731_create_jours_feries_table.php
+++ backend/database/migrations/2026_08_28_233731_create_jours_feries_table.php
@@ -6,20 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('jours_feries', function (Blueprint $table) {
             $table->id();
+            $table->string('nom');
+            $table->date('date');
+            $table->boolean('recurrent')->default(false);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('jours_feries');
