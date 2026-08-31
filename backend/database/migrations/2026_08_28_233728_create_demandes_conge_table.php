a389c829829310af88fea6490a4f46200857c699
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-08-31 12:30:29 +0100

    Remplire les clonnes de table demande_conge

--- backend/database/migrations/2026_08_28_233728_create_demandes_conge_table.php
+++ backend/database/migrations/2026_08_28_233728_create_demandes_conge_table.php
@@ -6,20 +6,24 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('demandes_conge', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('utilisateur_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('type_conge_id')->constrained('types_conge');
+            $table->date('date_debut');
+            $table->date('date_fin');
+            $table->enum('type_journee', ['demi_journee', 'journee_entiere'])
+                ->default('journee_entiere');
+            $table->text('motif')->nullable();
+            $table->enum('statut', ['pendingManager', 'pendingHR', 'approved', 'rejected'])
+                ->default('pendingManager');
+            $table->timestamp('date_creation')->useCurrent();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('demandes_conge');
