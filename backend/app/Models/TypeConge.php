f6e90077865517f1c843b0f89127bcc555af5395
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-02 10:47:48 +0100

    Créer les relations éloquantes entre dans le model TypeConge

--- backend/app/Models/TypeConge.php
+++ backend/app/Models/TypeConge.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TypeConge extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['nom', 'jour_reste_par_mois', 'necessite_justificatif'];
+
+    protected function casts(): array
+    {
+        return [
+            'necessite_justificatif' => 'boolean',
+        ];
+    }
+
+    public function demandesConge()
+    {
+        return $this->hasMany(DemandeConge::class, 'type_conge_id');
+    }
+
+    public function soldesConge()
+    {
+        return $this->hasMany(SoldeConge::class, 'type_conge_id');
+    }
 }
