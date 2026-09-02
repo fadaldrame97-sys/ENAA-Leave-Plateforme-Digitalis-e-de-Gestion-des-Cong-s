8154a3e79566cff6f64cf70bf68689d544161603
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-02 11:08:32 +0100

    Créer les relations éloquantes entre dans le model SoldeCongé

--- backend/app/Models/SoldeConge.php
+++ backend/app/Models/SoldeConge.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SoldeConge extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'utilisateur_id',
+        'type_conge_id',
+        'solde_acquis',
+        'solde_pris',
+        'annee',
+    ];
+
+    public function utilisateur()
+    {
+        return $this->belongsTo(User::class, 'utilisateur_id');
+    }
+
+    public function typeConge()
+    {
+        return $this->belongsTo(TypeConge::class, 'type_conge_id');
+    }
 }
