128f7b79b03e8009b3c23a1e8ae0ca291b26d1f9
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-02 11:01:46 +0100

    Créer les relations éloquantes entre dans le model justification

--- backend/app/Models/Justification.php
+++ backend/app/Models/Justification.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Justification extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'demande_conge_id',
+        'chemin_fichier',
+        'type_fichier',
+        'date_upload',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'date_upload' => 'datetime',
+        ];
+    }
+
+    public function demandeConge()
+    {
+        return $this->belongsTo(DemandeConge::class, 'demande_conge_id');
+    }
 }
