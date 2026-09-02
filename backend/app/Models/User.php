0d7b337f115e6a6d984db7d5ce75a333d6e558c1
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-02 10:33:14 +0100

    Créer les relations éloquantes entre dans le model user

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -2,46 +2,63 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
+        'prenom',
         'email',
         'password',
+        'role',
+        'active',
+        'departement_id',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'active' => 'boolean',
         ];
+    }
+
+    // Relations
+    public function departement()
+    {
+        return $this->belongsTo(Departement::class);
+    }
+
+    public function demandesConge()
+    {
+        return $this->hasMany(DemandeConge::class, 'utilisateur_id');
+    }
+
+    public function soldesConge()
+    {
+        return $this->hasMany(SoldeConge::class, 'utilisateur_id');
+    }
+
+    public function notifications()
+    {
+        return $this->hasMany(Notification::class, 'utilisateur_id');
+    }
+
+    public function plansRemplacement()
+    {
+        return $this->hasMany(PlanRemplacement::class, 'utilisateur_id');
     }
 }
