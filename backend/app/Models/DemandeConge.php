8de364d154999d9d8fb8839dfb3644777305f020
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-02 10:56:08 +0100

    Créer les relations éloquantes entre dans le model DemandeCongé

--- backend/app/Models/DemandeConge.php
+++ backend/app/Models/DemandeConge.php
@@ -2,9 +2,55 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DemandeConge extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'utilisateur_id',
+        'type_conge_id',
+        'date_debut',
+        'date_fin',
+        'type_journee',
+        'motif',
+        'statut',
+        'date_creation',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'date_debut' => 'date',
+            'date_fin' => 'date',
+            'date_creation' => 'datetime',
+        ];
+    }
+
+    public function utilisateur()
+    {
+        return $this->belongsTo(User::class, 'utilisateur_id');
+    }
+
+    public function typeConge()
+    {
+        return $this->belongsTo(TypeConge::class, 'type_conge_id');
+    }
+
+    public function justifications()
+    {
+        return $this->hasMany(Justification::class, 'demande_conge_id');
+    }
+
+    public function planRemplacement()
+    {
+        return $this->hasOne(PlanRemplacement::class, 'demande_conge_id');
+    }
+
+    public function notifications()
+    {
+        return $this->hasMany(Notification::class, 'demande_conge_id');
+    }
 }
