0f8c6ef0e53234245a9a2f7f6c50729d4a28e6dc
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-02 10:41:54 +0100

    Créer les relations éloquantes entre dans le model departement

--- backend/app/Models/Departement.php
+++ backend/app/Models/Departement.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Departement extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['nom', 'manager_id'];
+
+    public function utilisateurs()
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function manager()
+    {
+        return $this->belongsTo(User::class, 'manager_id');
+    }
 }
