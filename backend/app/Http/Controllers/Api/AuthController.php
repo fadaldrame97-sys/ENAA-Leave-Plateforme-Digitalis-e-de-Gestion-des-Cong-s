b47d1f6d7ed5776d72fcdf879aaa71d4ee0cf064
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-02 12:26:00 +0100

    appeler la logique de service

--- backend/app/Http/Controllers/Api/AuthController.php
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,32 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Services\AuthService;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
-}
+    public function __construct(protected AuthService $authService)
+    {
+    }
+
+    
+    public function login(LoginRequest $request)
+    {
+        $result = $this->authService->login(
+            $request->email,
+            $request->password
+        );
+
+        if (!$result['success']) {
+            return response()->json(['message' => $result['message']], 401);
+        }
+
+        return response()->json([
+            'user' => $result['user'],
+            'token' => $result['token'],
+        ]);
+    }
+
+     }
