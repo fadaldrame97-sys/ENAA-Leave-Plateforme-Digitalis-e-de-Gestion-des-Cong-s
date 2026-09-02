fb8b8d37cdd584f48ae5f570360d54fd7ea4de23
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-02 11:13:44 +0100

    Créer les relations éloquantes entre dans le model notification

--- backend/app/Models/Notification.php
+++ backend/app/Models/Notification.php
@@ -2,9 +2,37 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'utilisateur_id',
+        'demande_conge_id',
+        'message',
+        'type',
+        'lu',
+        'date_envoi',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'lu' => 'boolean',
+            'date_envoi' => 'datetime',
+        ];
+    }
+
+    public function utilisateur()
+    {
+        return $this->belongsTo(User::class, 'utilisateur_id');
+    }
+
+    public function demandeConge()
+    {
+        return $this->belongsTo(DemandeConge::class, 'demande_conge_id');
+    }
 }
