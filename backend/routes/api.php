49b162ca52c169b6014e122a38dcd6f5a0307371
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-04 12:26:02 +0100

    Créer toutes les routes : demandes conge pending manger pendingHR

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,10 +1,18 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\DemandeCongeController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::post('/demandes-conge', [DemandeCongeController::class, 'store']);
+    Route::get('/demandes-conge/me', [DemandeCongeController::class, 'mesRequetes']);
+    Route::get('/demandes-conge/pending-manager', [DemandeCongeController::class, 'enAttenteManager']);
+    Route::get('/demandes-conge/pending-hr', [DemandeCongeController::class, 'enAttenteRH']);
+    Route::post('/demandes-conge/{demandeConge}/approve', [DemandeCongeController::class, 'approuver']);
+    Route::post('/demandes-conge/{demandeConge}/reject', [DemandeCongeController::class, 'refuser']);
 });
