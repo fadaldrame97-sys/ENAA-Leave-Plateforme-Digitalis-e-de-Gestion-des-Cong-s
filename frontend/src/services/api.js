d27ec3ac51d13bf4b29e88312c9cf6489c246900
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-09-04 21:30:15 +0100

    Ajouter un obejet qui contient des function pour les taches de manger et HR

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -18,8 +18,24 @@ api.interceptors.request.use((config) => {
 
 
 export const leaveService = {
-  createRequest: (data) => api.post("/demandes-conge", data),
-  getMyRequests: () => api.get("/demandes-conge/me"),
+  createRequest: function (data) {
+    return api.post("/demandes-conge", data);
+  },
+  getMyRequests: function () {
+    return api.get("/demandes-conge/me");
+  },
+  getPendingManager: function () {
+    return api.get("/demandes-conge/pending-manager");
+  },
+  getPendingHR: function () {
+    return api.get("/demandes-conge/pending-hr");
+  },
+  approve: function (id) {
+    return api.post("/demandes-conge/" + id + "/approve");
+  },
+  reject: function (id) {
+    return api.post("/demandes-conge/" + id + "/reject");
+  },
 };
 
 export default api;
